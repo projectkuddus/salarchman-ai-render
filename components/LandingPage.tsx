18a50bf6290780755987eb2a1f745b14204d8811
Salarchman <86668451+Salarchman@users.noreply.github.com>
2025-12-17 23:12:47 +0600

    Update gallery with all 17 available images

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -10,9 +10,19 @@ const DEVELOPER_GALLERY = [
     "/gallery/archivision-1765933707253.jpg?v=1",
     "/gallery/salARCHman-1765944744165.jpg?v=1",
     "/gallery/salARCHman-1765956698737.jpg?v=1",
-    "/gallery/gallery-new-1.png?v=1",
-    "/gallery/gallery-new-2.png?v=1",
-    "/gallery/gallery-new-3.png?v=1"
+    "/gallery/gallery-upload-01.png?v=1",
+    "/gallery/gallery-upload-02.png?v=1",
+    "/gallery/gallery-upload-03.png?v=1",
+    "/gallery/gallery-upload-04.png?v=1",
+    "/gallery/gallery-upload-05.png?v=1",
+    "/gallery/gallery-upload-06.png?v=1",
+    "/gallery/gallery-upload-07.png?v=1",
+    "/gallery/gallery-upload-08.png?v=1",
+    "/gallery/gallery-upload-09.png?v=1",
+    "/gallery/gallery-upload-10.png?v=1",
+    "/gallery/gallery-upload-11.png?v=1",
+    "/gallery/gallery-upload-12.png?v=1",
+    "/gallery/gallery-upload-13.png?v=1"
 ];
 
 export const LandingPage: React.FC<LandingPageProps> = ({ onGetStarted }) => {
